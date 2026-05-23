7ef15d4007c6bae02efd81f900dad96c7d2fba2f
erickmalvaezc <erickmalvaez@ciencias.unam.mx>
2026-05-22 19:29:20 -0600

    subimos el código corregido y agregamos más comentarios

--- src/Practica05.hs
+++ src/Practica05.hs
@@ -2,32 +2,87 @@ module Practica05 where
 
 import Terminos
 
---Aplicar una sustitucion a un termino
+-- Sustituimos un termino
+-- Si el término es una variable, busca si está en la sustitución y la reemplaza.
+-- Si es una función, se sustituyen los argumentos
 apsubT :: Term -> Subst -> Term
-apsubT = undefined 
+apsubT (Var x) s =
+    case lookup x s of
+        Just t  -> t
+        Nothing -> Var x
+apsubT (Fun f args) s = Fun f (aplicarLista args s)
 
---Funcion auxiliar para aplicar la sustitucion a una lista de terminos
+-- Aplica una sustitución a cada término
 aplicarLista :: [Term] -> Subst -> [Term]
-aplicarLista = undefined 
+aplicarLista ts s = map (`apsubT` s) ts
 
---Funcion que elimina los pares que son de la forma x=x
+-- Elimina de una sustitución todos los pares triviales
 simpSus :: Subst -> Subst
-simpSus = undefined
+simpSus = filter (\(x, t) -> t /= Var x)
 
---Funcion que calcula la composicion de dos sustituciones
+-- Calcula la composición σ1σ2 de dos sustituciones.
+-- primero aplica σ1, luego σ2":
+-- Actualiza los términos de σ1 aplicándoles σ2.
+-- Agrega los pares de σ2 cuyas variables no están en el dominio de σ1.
+-- Luego simplifica eliminando pares triviales.
 compSus :: Subst -> Subst -> Subst
-compSus = undefined
+compSus s1 s2 =
+    simpSus $ s1actualizada ++ s2filtrada
+  where
+    -- Aplica s2 a cada s1
+    s1actualizada = [(x, apsubT t s2) | (x, t) <- s1]
+    domS1 = map fst s1
+    s2filtrada = [(x, t) | (x, t) <- s2, x `notElem` domS1]
 
---Funcion que devuelve un umg de dos terminos, si es que lo hay
+-- | Unifica dos términos. Devuelve una lista o lista vacia si no se pueden unir
 unifica :: Term -> Term -> [Subst]
-unifica = undefined
+unifica (Var x) (Var y)
+    | x == y    = [[]]                  
+    | otherwise = [[( x, Var y)]]       -- sustituir x por y
+unifica (Var x) t
+    | ocurre x t = []                   
+    | otherwise  = [[(x, t)]]
+unifica t (Var x)
+    | ocurre x t = []
+    | otherwise  = [[(x, t)]]
+unifica (Fun f args1) (Fun g args2)
+    | f /= g                        = []   -- distintos nombres de función
+    | length args1 /= length args2  = []   
+    | otherwise                     = unificaListas args1 args2
 
+-- Occur check: verifica si la variable x aparece en el termino t.
+ocurre :: Nombre -> Term -> Bool
+ocurre x (Var y)      = x == y
+ocurre x (Fun _ args) = any (ocurre x) args
 
---Funcion que devuelve un unificador de dos términos funcionales, si es que lo hay
+-- Unifica dos listas de componente a componente,
+-- componiendo las sustituciones parciales.
 unificaListas :: [Term] -> [Term] -> [Subst]
-unificaListas = undefined
+unificaListas [] [] = [[]]
+unificaListas (t1:ts1) (t2:ts2) =
+    case unifica t1 t2 of
+        []    -> []
+        [mu1] ->
+            -- Aplica mu1 al resto y unifica
+            case unificaListas (aplicarLista ts1 mu1) (aplicarLista ts2 mu1) of
+                []    -> []
+                [mu2] -> [compSus mu1 mu2]
+                _     -> []
+        _ -> []
+unificaListas _ _ = []
 
---Funcion que devuelve un umg de una lista de termino, si es que lo hay
+-- Unifica un conjunto de terminos.
+-- Estrategia: unifica los dos primeros, aplica el unificador al resto y repite
 unificaConj :: [Term] -> [Subst]
-unificaConj = undefined
-
+unificaConj []  = [[]]
+unificaConj [_] = [[]]
+unificaConj (t1:t2:ts) =
+    case unifica t1 t2 of
+        []    -> []
+        [mu1] ->
+            let resto = aplicarLista (t2:ts) mu1
+            in case unificaConj resto of
+                []    -> []
+                [mu2] -> [compSus mu1 mu2]
+                _     -> []
+        _ -> []
